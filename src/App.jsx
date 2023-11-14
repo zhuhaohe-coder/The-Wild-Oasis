38960eda95558509b1f45fb6800583d4b1135036
Haohe Zhu <2452466220@qq.com>
2023-11-14 15:40:40 +0800

    reset css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: var(--color-brand-200);
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <H1>The Wild Oasis</H1>
+      <Button>checkout</Button>
+      <Input placeholder="Number of guests" />
+    </>
+  );
 }
 
 export default App;
