1ecfea03efc8f76863b9283abe5de79f344e9b91
Haohe Zhu <2452466220@qq.com>
2023-11-15 19:19:35 +0800

    表单校验/组件封装

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -14,6 +14,20 @@ export async function deleteCabin(id) {
   const { error } = await supabase.from("cabins").delete().eq("id", id);
   if (error) {
     console.error(error);
-    throw new Error("Cabins could not be loaded");
+    throw new Error("Cabins could not be deleted");
   }
+}
+
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be created");
+  }
+
+  return data;
 }
