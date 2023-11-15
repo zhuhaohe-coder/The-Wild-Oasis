81b6a3578bba05afbd26f0bb36d8d0b5d4f6cb4a
Haohe Zhu <2452466220@qq.com>
2023-11-15 18:01:44 +0800

    使用react-hook-form管理表单

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,17 +1,27 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
+import { useState } from "react";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>TEST</p>
+        <p>Filter / Sort</p>
       </Row>
 
       <Row>
         <CabinTable />
+
+        <Button onClick={() => setShowForm((showForm) => !showForm)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
