2083d851aa6c16e6f7187c9c17b9e55bfc8c8cf9
Haohe Zhu <2452466220@qq.com>
2023-11-19 14:18:46 +0800

    feat_订单分页

--- src/features/bookings/hooks/useBookings.js
+++ src/features/bookings/hooks/useBookings.js
@@ -5,25 +5,30 @@ import { useSearchParams } from "react-router-dom";
 export function useBookings() {
   const [searchParams] = useSearchParams();
 
+  //过滤
   const filterValue = searchParams.get("status");
   const filter =
     !filterValue || filterValue === "all"
       ? null
       : { field: "status", value: filterValue };
 
+  //排序
   const sortByRow = searchParams.get("sortBy") || "startDate-desc";
   const [field, direction] = sortByRow.split("-");
   const sortBy = { field, direction };
 
+  //分页
+  const page = searchParams.get("page") < 1 ? 1 : +searchParams.get("page");
+
   const {
-    data: bookings,
+    data: { bookings, count } = {},
     isLoading,
     error,
   } = useQuery({
     //添加依赖, 变化时自动fetch数据
-    queryKey: ["bookings", filter, sortBy],
-    queryFn: () => getBookings({ filter, sortBy }),
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
-  return { bookings, isLoading, error };
+  return { bookings, count, isLoading, error };
 }
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,31 +1,38 @@
-import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
+import { getToday } from "../utils/helpers";
+import { PAGE_SIZE } from "../utils/constants";
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
     .from("bookings")
     .select(
-      "id, created_at,startDate,endDate,numNights,numGuests,status,totalPrice,cabins(name),guests(fullName,email)"
+      "id, created_at,startDate,endDate,numNights,numGuests,status,totalPrice,cabins(name),guests(fullName,email)",
+      { count: "exact" }
     );
 
   // FILTER
   if (filter) {
     query = query.eq(filter.field, filter.value);
   }
-
+  //SORT
   if (sortBy) {
     query = query.order(sortBy.field, {
       ascending: sortBy.direction === "asc",
     });
   }
-
-  const { data: bookings, error } = await query;
+  //PAGINATION
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
+    query = query.range(from, to);
+  }
+  const { data: bookings, error, count } = await query;
   if (error) {
     console.error(error);
     throw new Error("Bookings could not be loaded");
   }
 
-  return bookings;
+  return { bookings, count };
 }
 
 export async function getBooking(id) {
